534973b5bb861a34fc24fbdce182df39d9eda13e
Talha Chowdhury <talhachy@bennington.edu>
2021-11-21 05:23:33 -0500

    Create 1001.scm

---
+++ beecrowd solutions/1001.scm
@@ -0,0 +1,20 @@
+;; Problem Title: #Extremely Basic#
+
+#lang scheme
+
+(define x (read))
+(define y (read))
+(display "X = ")
+(display (+ x y))
+(newline)
+
+;;;; Test
+
+;; #Input#
+;; 10
+;; 9
+
+;;---
+
+;; #Output#
+;; X = 19
