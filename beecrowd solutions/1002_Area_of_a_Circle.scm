68158bed40372aa30aabe40bab1514013a3d0f73
Talha Chowdhury <talhachy@bennington.edu>
2021-11-21 06:41:39 -0500

    Create 1002_Area_of_a_Circle.scm

---
+++ beecrowd solutions/1002_Area_of_a_Circle.scm
@@ -0,0 +1,18 @@
+;; Problem Title: #Area of a Circle#
+
+#lang scheme
+
+(define r (read))
+(display "A=")
+(display (* 3.14159 r r))
+(newline)
+
+;;;; Test
+
+;; #Input#
+;; 2.00
+
+;;---
+
+;; #Output#
+;; A=12.56636
